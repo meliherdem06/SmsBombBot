911333561bdb81fae582b1e74eaabcaea6aac818
tingirifistik <51286195+tingirifistik@users.noreply.github.com>
2021-12-03 10:57:53 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -2,9 +2,15 @@ from colorama import Fore
 from request_sms import SendSms
 from time import sleep
 from os import system
+import platform
 
+os = platform.system()
+if os == "Windows":
+    clear = "cls"
+else:
+    clear = "clear"
 while 1:
-    system('cls')
+    system(clear)
     print(Fore.LIGHTCYAN_EX + """
      ______                         _     
     |  ____|                       | |    
@@ -18,52 +24,52 @@ while 1:
     try:
         menu = int(input(Fore.LIGHTMAGENTA_EX + "1. SMS Gönder\n2. Çıkış\n\n" + Fore.LIGHTYELLOW_EX + "Seçim: "))
     except ValueError:
-        system("cls")
+        system(clear)
         print(Fore.LIGHTRED_EX + "Hatalı giriş yaptınız. Tekrar deneyiniz.")
         sleep(3)
         continue
     if menu == 1:
-        system("cls")
+        system(clear)
         try:
             print(Fore.LIGHTYELLOW_EX + "Telefon numarasını başında '+90' olmadan yazınız: "+ Fore.LIGHTGREEN_EX, end="")
             tel_no = int(input())
             if len(str(tel_no)) != 10:
                 raise ValueError
         except ValueError:
-            system("cls")
+            system(clear)
             print(Fore.LIGHTRED_EX + "Hatalı telefon numarası. Tekrar deneyiniz.") 
             sleep(3)
             continue
-        system("cls")
+        system(clear)
         try:
             print(Fore.LIGHTYELLOW_EX + "Kaç adet SMS göndermek istiyorsun: "+ Fore.LIGHTGREEN_EX, end="")
             kere = int(input())
         except ValueError:
-            system("cls")
+            system(clear)
             print(Fore.LIGHTRED_EX + "Hatalı giriş yaptınız. Tekrar deneyiniz.") 
             sleep(3)
             continue
-        system("cls")
+        system(clear)
         try:
             print(Fore.LIGHTYELLOW_EX + "Kaç saniye aralıkla göndermek istiyorsun: "+ Fore.LIGHTGREEN_EX, end="")
             aralik = int(input())
         except ValueError:
-            system("cls")
+            system(clear)
             print(Fore.LIGHTRED_EX + "Hatalı giriş yaptınız. Tekrar deneyiniz.") 
             sleep(3)
             continue
         sms = SendSms(str(tel_no))
-        while sms.adet < kere+1:
+        while sms.adet < kere:
             for attribute in dir(SendSms):
                 attribute_value = getattr(SendSms, attribute)
                 if callable(attribute_value):
                     if attribute.startswith('__') == False:
-                        if sms.adet >= kere+1:
+                        if sms.adet >= kere:
                             break
                         exec("sms."+attribute+"()")
                         sleep(aralik)
     elif menu == 2:
-        system("cls")
+        system(clear)
         print(Fore.LIGHTRED_EX + "Çıkış yapılıyor...")
         sleep(2)
         break
